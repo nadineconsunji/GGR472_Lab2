70f5e82be55463da4ac3d1379150b0f3a5e17891
nadineconsunji <nadine.consunji@mail.utoronto.ca>
2026-02-10 15:30:15 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// mapbox token to link to my mapbox account 
+mapboxgl.accessToken = 'pk.eyJ1IjoibmFkaW5lY29uc3VuamkiLCJhIjoiY21rZWU1djI4MDV6NTNkb29meTJzMW81dSJ9.t6RLssyQkfZODRIMy_ToNQ';
+
+// map container will use my map style 
+const map = new mapboxgl.Map({
+	container: 'my-map', // map container ID
+	style: 'mapbox://styles/nadineconsunji/cmkyjcxru008t01s7c1xvf9fr', // style URL
+	center: [-79.40, 43.66], // starting position [lng, lat] to start centred around Toronto 
+	zoom: 12, // starting zoom
+});
+
+// Once map finishes loading, trigger the following functions 
+map.on('load', () => {
+
+// 1. Add data sources 
+	// Outdoor bike parking - GeoJSON file (added via URL for organisation)
+    map.addSource('outdoor-bike-parking', { // ID created
+        type: 'geojson',
+        data: 'https://nadineconsunji.github.io/GGR472_EX5/OutdoorBicycleParking.geojson' //CHANGE THIS RIGHT HERE!!! 
+
+	// Bike lanes - GeoJSON file (added via URL for organisation)
+    map.addSource('bike-lanes', { 
+        type: 'geojson',
+        url: 'https://nadineconsunji.github.io/GGR472_EX5/BikeRoutes.geojson' //CHANGE THIS RIGHT HERE!!! 
+    });
+
+// 2. Visualise data layers/load them into the map 
+	// Outdoor bike parking illustrated through points 
+    map.addLayer({
+        'id': 'outdoor-bike-parking-ppt', // ID created
+        'type': 'circle',
+        'source': 'outdoor-bike-parking',
+        'paint': {
+            'circle-radius': 6,
+            'circle-color': '#c90000'
+        }
+    });
+
+	// Bike lanes illustrated through lines 
+    map.addLayer({
+        id: 'bike-lanes-line',
+        type: 'line',
+        source: 'bike-lanes',
+        paint: {
+            'line-radius': 2, 
+            'line-color': '#007cbf'
+        }
+    });
+
+});
